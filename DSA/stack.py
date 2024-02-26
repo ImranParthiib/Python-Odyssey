010d2dcbc801ca16fe0ceb08b1c32c9421000467
ImranParthib <imranparthib13@gmail.com>
2024-02-26 14:41:46 +0600

    commented stack.py
    stack
    
    Co-Authored-By: Al Imran <71823101+ImranAvenger@users.noreply.github.com>

--- DSA/stack.py
+++ DSA/stack.py
@@ -24,10 +24,10 @@ def pop(stack):
     return stack.pop()
 
 
-stack = create_stack()
-push(stack, str(1))
-push(stack, str(2))
-push(stack, str(3))
-push(stack, str(4))
-print("popped item: " + pop(stack))
-print("stack after popping an element: " + str(stack))
+# stack = create_stack()
+# push(stack, str(1))
+# push(stack, str(2))
+# push(stack, str(3))
+# push(stack, str(4))
+# print("popped item: " + pop(stack))
+# print("stack after popping an element: " + str(stack))
